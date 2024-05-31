fabbe19211b1836d8190c0fd3f30f7df02520164
SandraMaylii <srodriguezg8@upao.edu.pe>
2024-05-31 17:28:46 -0500

    Update Pantalla Registro

--- app/src/main/java/com/upao/panaderia/RegisterActivity.kt
+++ app/src/main/java/com/upao/panaderia/RegisterActivity.kt
@@ -1,20 +1,73 @@
 package com.upao.panaderia
 
+
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.upao.panaderia.R
+import com.upao.panaderia.databinding.ActivityRegisterBinding
+import com.upao.panaderia.register.RegisterClientFragment
+import com.upao.panaderia.register.RegisterSellerFragment
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loadFragment(RegisterClientFragment())
+        }
+
+        initializeButtons()
+
+        binding.registerClient.setOnClickListener {
+            updateButtonStates(isClientSelected = true)
+            loadFragment(RegisterClientFragment())
+        }
+
+        binding.registerSeller.setOnClickListener {
+            updateButtonStates(isClientSelected = false)
+            loadFragment(RegisterSellerFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
+    }
+
+    private fun initializeButtons() {
+        binding.registerClient.setBackgroundResource(R.drawable.button_state_selected)
+        binding.registerSeller.setBackgroundResource(R.drawable.button_state_selected)
+        binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+        binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.no_select)
+    }
+
+    private fun updateButtonStates(isClientSelected: Boolean) {
+        if (isClientSelected) {
+            binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+            binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.no_select)
+            binding.registerClient.setTextAppearance(R.style.ButtonSelected)
+            binding.registerSeller.setTextAppearance(R.style.ButtonNoSelected)
+        } else {
+            binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+            binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.no_select)
+            binding.registerSeller.setTextAppearance(R.style.ButtonSelected)
+            binding.registerClient.setTextAppearance(R.style.ButtonNoSelected)
         }
     }
 }
